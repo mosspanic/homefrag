8a39525345d8509b69b8b01823f897aa816b93ab
mosspanic <daw_dash@mail.ru>
2021-09-08 09:23:11 +0500

    2 big font

--- app/src/main/java/com/example/homefrag/MainActivity.kt
+++ app/src/main/java/com/example/homefrag/MainActivity.kt
@@ -2,18 +2,20 @@ package com.example.homefrag
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .add(R.id.main_container, BFragment1())
-            .commit()
-        Thread.sleep(7000)
+        supportFragmentManager.beginTransaction().add(R.id.main_container, BFragment1()).commit()
+        //Thread.sleep(7000)
+
         supportFragmentManager.beginTransaction()
             .add(R.id.main_container, BFragment2())
             .commit()
     }
 
 }
+
+
